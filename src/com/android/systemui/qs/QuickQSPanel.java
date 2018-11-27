235b95217ffb889e053ffb863cc84df20540fa88
tangshiyuan <tang.shiyuan@jancar.com.cn>
2018-11-27 18:53:35 +0800

    快速设置修改

--- src/com/android/systemui/qs/QuickQSPanel.java
+++ src/com/android/systemui/qs/QuickQSPanel.java
@@ -186,8 +186,8 @@ public class QuickQSPanel extends QSPanel {
 
         private LayoutParams generateSpaceParams() {
             int size = mContext.getResources().getDimensionPixelSize(R.dimen.qs_quick_tile_size);
-            LayoutParams lp = new LayoutParams(0, size);
-            lp.weight = 1f/6f;
+            LayoutParams lp = new LayoutParams(170, size);
+//            lp.weight = 1f/6f;
             lp.gravity = Gravity.CENTER;
             return lp;
         }
