25ab2a1d6cc81346372a18f77cd8ebd2ab632724
tangshiyuan <tang.shiyuan@jancar.com.cn>
2018-12-06 09:48:05 +0800

    修改亮度条音量条转场动画

--- src/com/android/systemui/volume/VolumeDialogImpl.java
+++ src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -595,11 +595,11 @@ public class VolumeDialogImpl implements VolumeDialog, TunerService.Tunable {
         if (D.BUG) Log.d(TAG, "updateExpandedH " + expanded);
         updateExpandButtonH();
         updateFooterH();
-        TransitionManager.endTransitions(mDialogView);
+//        TransitionManager.endTransitions(mDialogView);
         final VolumeRow activeRow = getActiveRow();
         if (!dismissing) {
             mWindow.setLayout(mWindow.getAttributes().width, ViewGroup.LayoutParams.MATCH_PARENT);
-            TransitionManager.beginDelayedTransition(mDialogView, getTransition());
+//            TransitionManager.beginDelayedTransition(mDialogView, getTransition());
         }
         updateRowsH(activeRow);
         rescheduleTimeoutH();
@@ -728,8 +728,8 @@ public class VolumeDialogImpl implements VolumeDialog, TunerService.Tunable {
                 && (mAudioManager.isStreamAffectedByRingerMode(mActiveStream) || mExpanded)
                 && !mZenPanel.isEditing();
 
-        TransitionManager.endTransitions(mDialogView);
-        TransitionManager.beginDelayedTransition(mDialogView, getTransition());
+//        TransitionManager.endTransitions(mDialogView);
+//        TransitionManager.beginDelayedTransition(mDialogView, getTransition());
         if (wasVisible != visible && !visible) {
             prepareForCollapse();
         }
