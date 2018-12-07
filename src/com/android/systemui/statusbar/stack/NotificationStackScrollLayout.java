d878551219c98ad70c785d68ab2000af65493938
flycnzebra <tang.shiyuan@jancar.com.cn>
2018-12-07 15:28:25 +0800

    修改通知栏背景

--- src/com/android/systemui/statusbar/stack/NotificationStackScrollLayout.java
+++ src/com/android/systemui/statusbar/stack/NotificationStackScrollLayout.java
@@ -4899,6 +4899,7 @@ public class NotificationStackScrollLayout extends ViewGroup
     @Override
     public void addView(View child) {
         FlyLog.d("ADD VIEW child="+child);
+        child.setBackgroundColor(0x3fffff00);
         super.addView(child);
     }
 }
