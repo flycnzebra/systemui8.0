59ffe27239fc8002945d4d9aa1a5466ec7680c41
flycnzebra <tang.shiyuan@jancar.com.cn>
2018-12-07 10:08:09 +0800

    开启DEBUG

--- src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -437,7 +437,6 @@ public class NotificationPanelView extends PanelView implements
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
-        FlyLog.d("onLayout");
         setIsFullWidth(mNotificationStackScroller.getWidth() == getWidth());
 
         // Update Clock Pivot
