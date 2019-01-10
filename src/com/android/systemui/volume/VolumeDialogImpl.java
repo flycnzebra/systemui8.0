7dd60250d5257c3cd3ce5cabacd64bd63d35431e
flycnzebra <tang.shiyuan@jancar.com.cn>
2019-01-10 17:26:03 +0800

    修改SystemUI解mute

--- src/com/android/systemui/volume/VolumeDialogImpl.java
+++ src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -1313,7 +1313,11 @@ public class VolumeDialogImpl implements VolumeDialog, TunerService.Tunable {
         @Override
         public void onStartTrackingTouch(SeekBar seekBar) {
             if (D.BUG) Log.d(TAG, "onStartTrackingTouch"+ " " + mRow.stream);
-            mAudioManager.adjustStreamVolume(mRow.stream,AudioManager.ADJUST_UNMUTE,0);
+            try {
+                mAudioManager.adjustStreamVolume(mRow.stream, AudioManager.ADJUST_UNMUTE, 0);
+            }catch (Exception e){
+                FlyLog.e(e.toString());
+            }
             mController.setActiveStream(mRow.stream);
             mRow.tracking = true;
         }
