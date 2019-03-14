1d2611f635fdd6eda89ba9249fd95fe6204b8c5c
flycnzebra <tang.shiyuan@jancar.com.cn>
2019-03-14 10:54:03 +0800

    修改SystemUI状态栏

---
+++ src/com/android/systemui/jancar/JancarMenu.java
@@ -0,0 +1,104 @@
+package com.android.systemui.jancar;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.systemui.R;
+import com.android.systemui.settings.BrightnessController;
+import com.android.systemui.settings.ToggleSliderView;
+
+/**
+ * A dialog that provides controls for adjusting the screen brightness.
+ */
+public class JancarMenu extends Activity {
+    private boolean isStop = true;
+    private Handler mHander = new Handler() {
+        public void handleMessage(Message m) {
+            super.handleMessage(m);
+            switch (m.what) {
+                case 1:
+                    if (!isStop) {
+                        removeCallbacks(hideUI);
+                        postDelayed(hideUI, 3000);
+                    }
+                    break;
+            }
+        }
+    };
+    private Runnable hideUI = new Runnable() {
+        public void run() {
+            finish();
+            mHander.postDelayed(hideUI, 3000);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final Window window = getWindow();
+
+        window.setGravity(Gravity.CENTER);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        window.requestFeature(Window.FEATURE_NO_TITLE);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        setContentView(R.layout.jancar_menu);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        isStop = false;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        isStop = true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        mHander.removeCallbacksAndMessages(null);
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN
+                || keyCode == KeyEvent.KEYCODE_VOLUME_UP
+                || keyCode == KeyEvent.KEYCODE_VOLUME_MUTE) {
+            finish();
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        View view = getWindow().getDecorView();
+        WindowManager.LayoutParams lp = (WindowManager.LayoutParams) view.getLayoutParams();
+        lp.y = lp.y - 30;
+        getWindowManager().updateViewLayout(view, lp);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(0,0);
+    }
+}
